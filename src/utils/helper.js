8b1f7e1b627bb1ef333e0cdfb948fc29575427cd
Nedu Backend <nedubackend@Nedus-MacBook-Pro.local>
2025-01-04 13:40:10 +0100

    first commit

---
+++ src/utils/helper.js
@@ -0,0 +1,30 @@
+export const getModelType = (modelString) => {
+    const modelMap = {
+      "App\\Models\\SuperAdmin": "super admin",
+      "App\\Models\\Admin": "manager",
+      "App\\Models\\Auditor": "auditor",
+      "App\\Models\\Marketer": "marketer"
+    };
+  
+    return modelMap[modelString] || "Unknown model";
+}
+
+export const getModelColor = (modelString) => {
+    const modelMap = {
+      "App\\Models\\SuperAdmin": "error",
+      "App\\Models\\Admin": "warning",
+      "App\\Models\\Auditor": "unknown",
+      "App\\Models\\Marketer": "primary"
+    };
+  
+    return modelMap[modelString] || "unknown";
+}
+
+export const getPaymentMethod = (modelString) => {
+  const modelMap = {
+    "cash": "error",
+    "bank": "primary"
+  };
+
+  return modelMap[modelString] || "unknown";
+}
